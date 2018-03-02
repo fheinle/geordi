d0bcc1d9bfc96267444240e20e5f51dc0f015741
Florian Heinle <florian.heinle@makandra.de>
2018-03-02 14:07:43 +0100

    allow setting ports or sockets to use
    This is useful when using multiple db software versions locally

--- lib/geordi/commands/db_clean.rb
+++ lib/geordi/commands/db_clean.rb
@@ -5,12 +5,42 @@ Drop non-whitelisted databases from local installations of MySQL/MariaDB and
   PostgreSQL. Offers to edit the whitelist.
 LONGDESC
 
+option :postgres_only, :aliases => '-P', :type => :boolean,
+  :desc => 'Only clean Postgres', :default => false
+option :mysql_only, :aliases => '-M', :type => :boolean,
+  :desc => 'Only clean MySQL/MariaDB', :default => false
+option :postgres, :banner => 'STRING',
+  :desc => 'Use Postgres port or socket'
+option :mysql, :banner => 'STRING',
+  :desc => 'Use MySQL/MariaDB port or socket'
+
 def drop_databases
   require 'geordi/db_cleaner'
   cleaner = DBCleaner.new
-  cleaner.clean_mysql
-  cleaner.clean_postgres
+  fail '-P and -M are mutually exclusive' if options.postgres_only and options.mysql_only
+  unless options.postgres_only
+    mysql_flags = nil
+    unless options.mysql.nil?
+      begin
+        mysql_port = Integer(options.mysql)
+        mysql_flags = "--port=#{mysql_port} --protocol=TCP"
+      rescue AttributeError
+        unless File.exist? options.mysql
+          fail "Path #{options.mysql} is not a valid MySQL socket"
+        end
+        mysql_flags = "--socket=#{options.mysql}"
+      end
+    end
+    cleaner.clean_mysql(mysql_flags)
+  end
 
+  unless options.mysql_only
+    postgres_flags = nil
+    unless options.postgres.nil?
+      postgres_flags = "--port=#{options.postgres}"
+    end
+    cleaner.clean_postgres(postgres_flags)
+  end
   # For formatted output, see geordi/interaction.rb
   success 'Done.'
 end
--- lib/geordi/db_cleaner.rb
+++ lib/geordi/db_cleaner.rb
@@ -1,4 +1,6 @@
 require 'fileutils'
+require 'socket'
+require 'open3'
 
 module Geordi
   class DBCleaner
@@ -25,20 +27,36 @@ module Geordi
       end
     end
 
-    def clean_mysql
+    def clean_mysql(extra_flags)
       announce 'Dropping MySQL and MariaDB databases'
       note 'Trying to authenticate using system root account (for MariaDB and MySQL from recent Oracle repos)'
-      mysql_command = 'sudo mysql'
-      database_list = list_all_mysql_dbs(mysql_command)
-      if $? == 1
-        note 'System authentication failed (no problem!), trying MySQL authentication'
-        mysql_command = 'mysql -uroot'
-        unless File.exist? File.join(Dir.home, 'my.cnf')
-          warn 'You do not have $HOME/.my.cnf. You will have to enter your MySQL root password a lot!'
-          mysql_command << ' -p'
+      unless extra_flags.nil?
+        if extra_flags.include? 'port'
+          port = Integer(extra_flags.split('=')[1].split()[0])
+          fail "Port #{port} is not open" unless is_port_open? port
         end
-        database_list = list_all_mysql_dbs(mysql_command) # retry
       end
+      mysql_command = 'sudo mysql'
+      mysql_command << " #{extra_flags}" unless extra_flags.nil?
+      Open3.popen3("#{mysql_command} -e 'QUIT'") do |stdin, stdout, stderr, thread|
+        break if thread.value.exitstatus == 0
+        mysql_error = stderr.read.lines[0].chomp.strip.split[1]
+        if mysql_error == '1045'
+          note 'System authentication failed (no problem!), trying MySQL authentication'
+          mysql_command = 'mysql -uroot'
+          mysql_command << " #{extra_flags}" unless extra_flags.nil?
+          unless File.exist? File.join(Dir.home, 'my.cnf')
+            warn 'You do not have $HOME/.my.cnf. You will have to enter your MySQL root password a lot!'
+            mysql_command << ' -p'
+          end
+          Open3.popen3("#{mysql_command} -e 'QUIT'") do |stdin2, stdout2, stderr2, thread2|
+            fail 'Could not connect to MySQL/MariaDB' unless thread2.value.exitstatus == 0
+          end
+        elsif mysql_error == '2013'
+          fail 'MySQL/MariaDB connection failed, is this the correct port?'
+        end
+      end
+      database_list = list_all_mysql_dbs(mysql_command)
       # confirm_deletion includes option for whitelist editing
       deletable_dbs = confirm_deletion('mysql', database_list)
       return if deletable_dbs.nil?
@@ -47,14 +65,28 @@ module Geordi
       end
     end
 
-    def clean_postgres
+    def clean_postgres(extra_flags)
       announce 'Dropping PostgreSQL databases'
-      database_list = `sudo -u postgres psql -l -t -A -c 'SELECT DATNAME FROM pg_database WHERE datistemplate = false;'`.split
+      postgres_command = 'sudo -u postgres psql'
+      unless extra_flags.nil?
+        begin
+          port = Integer(extra_flags.split('=')[1])
+          fail "Port #{port} is not open" unless is_port_open? port
+        rescue ArgumentError
+          socket = extra_flags.split('=')[1]
+          fail "Socket #{socket} does not exist" unless File.exist? socket
+        end
+        postgres_command << " #{extra_flags}"
+      end
+      Open3.popen3("#{postgres_command} -c ';'") do |stdin, stdout, stderr, thread|
+        fail 'Could not connect to PostgreSQL' unless thread.value.exitstatus == 0
+      end
+      database_list = `#{postgres_command} -t -A -c 'SELECT DATNAME FROM pg_database WHERE datistemplate = false;'`.split
       deletable_dbs = confirm_deletion('postgres', database_list)
       return if deletable_dbs.nil?
       deletable_dbs.each do |db|
         note "Dropping database `#{db}`."
-        `sudo -u postgres psql -c 'DROP DATABASE "#{db}";'`
+        `#{postgres_command} -c 'DROP DATABASE "#{db}";'`
       end
     end
 
@@ -138,4 +170,14 @@ end
 def cmd_exists? cmd
   system("which #{cmd} > /dev/null")
   return $?.exitstatus.zero?
+end
+
+def is_port_open?(port)
+  begin
+    socket = TCPSocket.new('127.0.0.1', port)
+    socket.close
+    return true
+  rescue Errno::ECONNREFUSED
+    return false
+  end
 end
